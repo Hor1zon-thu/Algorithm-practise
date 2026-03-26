47805114560ae39f7081193eef8b5a4836d59a96
Hor1zon <dcx24@mails.tsinghua.edu.cn>
2026-03-26 23:23:09 +0800

    dp

---
+++ leetcode/动态规划/no118 杨辉三角.py
@@ -0,0 +1,15 @@
+from typing import List
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 0 :
+            return []
+
+        result  = [[1]]
+        for i in range(1,numRows):
+            prev_row = result[i-1]
+            curr_row = [1]
+            for j in range (1,i):
+                curr_row.append(prev_row[j-1] + prev_row[j])
+            curr_row.append(1)
+            result.append(curr_row)
+        return result
